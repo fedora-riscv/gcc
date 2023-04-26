ca788bfdffb0f4bd7b210e848f2007cec8829de9
Jakub Jelinek <jakub@redhat.com>
2023-04-26 10:58:06 +0200

    13.1.1-1

--- update-gcc.sh
+++ update-gcc.sh
@@ -3,5 +3,5 @@
 git clone --depth 1 git://gcc.gnu.org/git/gcc.git gcc-dir.tmp
 git --git-dir=gcc-dir.tmp/.git fetch --depth 1 origin $1
 d=`date --iso | sed 's/-//g'`
-git --git-dir=gcc-dir.tmp/.git archive --prefix=gcc-13.0.1-$d/ $1 | xz -9e > gcc-13.0.1-$d.tar.xz
+git --git-dir=gcc-dir.tmp/.git archive --prefix=gcc-13.1.1-$d/ $1 | xz -9e > gcc-13.1.1-$d.tar.xz
 rm -rf gcc-dir.tmp
